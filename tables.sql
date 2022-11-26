f1c98b1c631390eb5d72415a2a8d609ed017a5ea
demoulin7 <bosonhiggs@outlook.fr>
2022-11-26 16:42:33 +0100

    mise à jour bdd

--- tables.sql
+++ tables.sql
@@ -1,25 +1,64 @@
-CREATE TABLE `users` (
-  `id` int AUTO_INCREMENT NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `birthday` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 26 nov. 2022 à 16:41
+-- Version du serveur :  10.4.16-MariaDB
+-- Version de PHP : 7.4.12
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
-(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
-(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
-(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `carpooling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annonces`
+--
+
+CREATE TABLE `annonces` (
+  `id` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `trajet` varchar(255) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `carId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `annonces`
+--
+
+INSERT INTO `annonces` (`id`, `prix`, `trajet`, `userId`, `carId`) VALUES
+(1, 45, 'Nancy=>Metz', 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cars`
+--
 
 CREATE TABLE `cars` (
-  `id` int AUTO_INCREMENT NOT NULL,
+  `id` int(11) NOT NULL,
   `brand` varchar(255) NOT NULL,
   `model` varchar(255) NOT NULL,
   `color` varchar(255) NOT NULL,
-  `nbrSlots` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `nbrSlots` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `cars`
+--
 
 INSERT INTO `cars` (`id`, `brand`, `model`, `color`, `nbrSlots`) VALUES
 (1, 'Skoda', 'Fabia', 'Noire', 5),
@@ -27,14 +66,137 @@ INSERT INTO `cars` (`id`, `brand`, `model`, `color`, `nbrSlots`) VALUES
 (3, 'Mercedes', 'Classe C', 'Noire', 4),
 (4, 'Renaut', 'Zoé', 'Bleu', 2);
 
-CREATE TABLE users_cars (
-	user_id INT NOT NULL, 
-	car_id INT NOT NULL, 
-	PRIMARY KEY(user_id, car_id)
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `passagers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthday` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
+(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08 00:00:00'),
+(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08 00:00:00'),
+(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_cars`
+--
+
+CREATE TABLE `users_cars` (
+  `user_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users_cars`
+--
 
 INSERT INTO `users_cars` (`user_id`, `car_id`) VALUES
 (1, 1),
 (1, 2),
 (2, 3),
 (3, 4);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_foreign_car` (`carId`),
+  ADD KEY `fk_foreign_user` (`userId`);
+
+--
+-- Index pour la table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users_cars`
+--
+ALTER TABLE `users_cars`
+  ADD PRIMARY KEY (`user_id`,`car_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD CONSTRAINT `fk_foreign_car` FOREIGN KEY (`carId`) REFERENCES `cars` (`id`),
+  ADD CONSTRAINT `fk_foreign_user` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
